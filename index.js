82fb23fa6d55f34ace505d4b4b7b8f5993be7c96
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-07-08 20:58:20 +0300

    changed index.js added express example

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!!')
+})
+let courses = [
+    {id: 1, title:"js"},
+    {id: 2, title:"html"},
+    {id: 3, title:"nodejs"}
+]
+
+// read
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+// create
+app.post('/courses', (req, res) => {
+    courses.push({
+        id:+(new Date()),
+        title: req.body.title
+    })
+    res.send(courses)
+})
+
+// delete
+app.delete('/courses/:id', (req, res) => {
+    const id = +req.params.id;
+
+    courses = courses.filter(c=> c.id !== id)
+
+    res.send(courses)
+})
+
+// update
+app.put('/courses/:id', (req, res) => {
+    const id = +req.params.id;
+
+    const course = courses.find(c=> c.id === id)
+        course.title =req.body.title
+    res.send(courses)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
